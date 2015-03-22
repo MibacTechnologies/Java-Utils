061b3942308495ef6cf976cc17fbf26758885ddb
Michael <mibac831@wp.pl>
2015-03-22 19:42:45 +0100

    Update javadoc.sh

--- javadoc.sh
+++ javadoc.sh
@@ -1,16 +1,20 @@
-if [ "$TRAVIS_REPO_SLUG" == "MibacTechnologies/Java-Utils" ] && \
+if [ "$TRAVIS_REPO_SLUG" == "google/truth" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Publishing javadoc...\n"
   
-  
+  mvn javadoc:aggregate
+  TARGET="$(pwd)/target"
+
   cd $HOME
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/MibacTechnologies/Java-Utils gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/google/truth gh-pages > /dev/null
   
   cd gh-pages
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git rm -rf api/latest 
+  mv ${TARGET}/site/apidocs api/latest
   git add -A -f api/latest
   git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
